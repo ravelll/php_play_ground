a4f5dba99ed896d05416e43df49deca15552c811
ravelll <menchikats@gmail.com>
2016-06-23 22:49:57 +0900

    php が実行できるようになるところまで

--- cookbooks/php/install.rb
+++ cookbooks/php/install.rb
@@ -2,7 +2,9 @@ include_recipe './phpenv_installer.rb'
 
 execute 'install php' do
   user 'root'
-  command 'export PATH=$PATH:/usr/local/phpenv/bin/;\
+  command 'echo "PATH=$PATH:/usr/local/phpenv/bin/:/usr/local/phpenv/versions/7.0.7/bin" >> /etc/profile;\
+  source /etc/profile;\
   phpenv install 7.0.7;\
   phpenv global 7.0.7'
+  not_if 'test -d /usr/local/phpenv/versions/7.0.7'
 end
--- cookbooks/php/phpenv_installer.rb
+++ cookbooks/php/phpenv_installer.rb
@@ -4,8 +4,3 @@ execute 'git clone phpenv-installer' do
   | PHPENV_ROOT=/usr/local/phpenv bash'
   not_if 'test -x /usr/local/phpenv/bin/phpenv'
 end
-
-# execute 'add phpenv bin to PATH' do
-#   user 'root'
-#   command 'PATH=$PATH:/usr/local/bin/phpenv/bin/phpenv'
-# end
