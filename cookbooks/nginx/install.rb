0797d3e426decccbe059d0511ce556ec8590b939
ravelll <menchikats@gmail.com>
2016-06-30 20:20:04 +0900

    putting files must exec after installing nginx

--- cookbooks/nginx/install.rb
+++ cookbooks/nginx/install.rb
@@ -1,9 +1,9 @@
-remote_file '/etc/nginx/nginx.conf'
-remote_file '/etc/nginx/conf.d/php_fpm.conf'
-
 package 'nginx' do
   action :install
 end
+
+remote_file '/etc/nginx/nginx.conf'
+remote_file '/etc/nginx/conf.d/php_fpm.conf'
 
 service 'nginx' do
   action [:enable, :start]
