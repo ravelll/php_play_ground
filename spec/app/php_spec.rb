965fcf47a3a98d7ba543650496f934d33aad6eed
ravelll <menchikats@gmail.com>
2016-07-01 16:09:37 +0900

    Fix php spec to update php 7.0.8

--- spec/app/php_spec.rb
+++ spec/app/php_spec.rb
@@ -1,10 +1,5 @@
 require 'spec_helper'
 
-describe file('/usr/local/phpenv/versions/7.0.7/bin/php') do
+describe file('/usr/local/phpenv/versions/7.0.8/bin/php') do
   it { should be_file }
-end
-
-describe service('php-fpm') do
-  it { should be_enabled }
-  it { should be_running }
 end
